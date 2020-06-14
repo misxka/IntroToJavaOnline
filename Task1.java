8a0ccf6fbaded89dbc7f508b8653f55c971bbec0
misxka <57208499+misxka@users.noreply.github.com>
2020-06-14 18:01:37 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int z = ((a - 3) * b / 2) + c;
+        System.out.println(z);
+    }
+}
