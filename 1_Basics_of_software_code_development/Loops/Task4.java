5a256f46c60d7ef5c293032e54e82145437a5b49
misxka <57208499+misxka@users.noreply.github.com>
2020-06-15 11:39:41 +0300

    Update Task4.java

--- 1_Basics_of_software_code_development/Loops/Task4.java
+++ 1_Basics_of_software_code_development/Loops/Task4.java
@@ -1,10 +1,9 @@
-import java.io.IOException;
 import java.math.BigDecimal;
 
 import static java.lang.Math.*;
 
 public class Task4 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         BigDecimal product = new BigDecimal("1");
         for(int i = 1; i <= 200; i++) {
             product = product.multiply(new BigDecimal(String.valueOf(pow(i, 2))));
