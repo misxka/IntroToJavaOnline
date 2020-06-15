a2dd40c6705b2d21a9b72024bb9dc83b2470be57
misxka <57208499+misxka@users.noreply.github.com>
2020-06-15 10:59:08 +0300

    Update Task3.java

--- 1_Basics_of_software_code_development/Loops/Task3.java
+++ 1_Basics_of_software_code_development/Loops/Task3.java
@@ -1,11 +1,8 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import static java.lang.Math.*;
 
 public class Task3 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int sum = 0;
         for(int i = 1; i <= 100; i++) {
             sum += pow(i, 2);
