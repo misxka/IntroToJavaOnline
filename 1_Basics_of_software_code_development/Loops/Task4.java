43587e86f42f33b7e785784d033fc19dcb8f014a
misxka <57208499+misxka@users.noreply.github.com>
2020-06-15 11:09:17 +0300

    Update Task4.java

--- 1_Basics_of_software_code_development/Loops/Task4.java
+++ 1_Basics_of_software_code_development/Loops/Task4.java
@@ -1,13 +1,10 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
 import static java.lang.Math.*;
 
 public class Task4 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BigDecimal product = new BigDecimal("1");
         for(int i = 1; i <= 200; i++) {
             product = product.multiply(new BigDecimal(String.valueOf(pow(i, 2))));
