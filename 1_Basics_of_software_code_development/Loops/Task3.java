d7e42a832973f45693f2f91352e53170c52ade63
misxka <57208499+misxka@users.noreply.github.com>
2020-06-15 11:40:17 +0300

    Update Task3.java

--- 1_Basics_of_software_code_development/Loops/Task3.java
+++ 1_Basics_of_software_code_development/Loops/Task3.java
@@ -1,8 +1,7 @@
-import java.io.IOException;
 import static java.lang.Math.*;
 
 public class Task3 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int sum = 0;
         for(int i = 1; i <= 100; i++) {
             sum += pow(i, 2);
