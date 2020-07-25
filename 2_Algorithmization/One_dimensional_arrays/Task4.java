8cd48a76be20c8f6be26811cf03f455d2cc04fc1
misxka <57208499+misxka@users.noreply.github.com>
2020-07-25 18:41:42 +0300

    Create Task4.java

---
+++ 2_Algorithmization/One_dimensional_arrays/Task4.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        double array[] = {12.1, -13.2, -14.3, -15.4, 0, -17.6, 0, 19.8, 20.9, 21.12};
+        double max = array[0];
+        double min = array[0];
+        int maxPosition = 0;
+        int minPosition = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > max) {
+                max = array[i];
+                maxPosition = i;
+            }
+            if(array[i] < min) {
+                min = array[i];
+                minPosition = i;
+            }
+        }
+        double tmp = array[minPosition];
+        array[minPosition] = array[maxPosition];
+        array[maxPosition] = tmp;
+        for(double element: array) {
+            System.out.print(element + " ");
+        }
+    }
+}
