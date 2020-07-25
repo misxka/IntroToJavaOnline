6bf53f70e82c30c147bf8d8111c81af7c0ee6d49
misxka <57208499+misxka@users.noreply.github.com>
2020-07-25 18:33:15 +0300

    Create Task3.java

---
+++ 2_Algorithmization/One_dimensional_arrays/Task3.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        double array[] = {12.1, -13.2, -14.3, -15.4, 0, -17.6, 0, 19.8, 20.9, 21.12};
+        int positiveSum = 0, negativeSum = 0, nullSum = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > 0) {
+                positiveSum++;
+            }
+            else if(array[i] == 0) {
+                nullSum++;
+            }
+            else negativeSum++;
+        }
+        System.out.println("Positive " + positiveSum);
+        System.out.println("Nulls " + nullSum);
+        System.out.println("Negative " + negativeSum);
+    }
+}
