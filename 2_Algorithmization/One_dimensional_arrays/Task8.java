01786e1605238dc3bab3e72e77c27f54ac47397c
misxka <57208499+misxka@users.noreply.github.com>
2020-07-26 13:08:22 +0300

    Create Task8.java

---
+++ 2_Algorithmization/One_dimensional_arrays/Task8.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        int array[] = {12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
+        int min = array[0];
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] < min) {
+                min = array[i];
+            }
+        }
+        int newSize = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] != min) newSize++;
+        }
+        int newArray[] = new int[newSize];
+        int i = 0;
+        for(int element: array) {
+            if(element != min) {
+                newArray[i] = element;
+                i++;
+            }
+        }
+        for(int element: newArray) {
+            System.out.println(element);
+        }
+    }
+}
