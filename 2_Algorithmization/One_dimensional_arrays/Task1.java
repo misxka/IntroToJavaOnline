f519e2e6a2562c8988ec58f54213e639b259af98
misxka <57208499+misxka@users.noreply.github.com>
2020-07-27 11:23:39 +0300

    Update Task1.java

--- 2_Algorithmization/One_dimensional_arrays/Task1.java
+++ 2_Algorithmization/One_dimensional_arrays/Task1.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Task1 {
     public static void main(String args[]) throws IOException {
         int array[] = {12, 55, 32, 7, 54, 29, 28, 14, 15, 91};
         BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
