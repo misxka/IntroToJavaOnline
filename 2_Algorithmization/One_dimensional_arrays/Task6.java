98b9094c95f2bd315a8bda905c759af31269ec4c
misxka <57208499+misxka@users.noreply.github.com>
2020-07-27 11:25:48 +0300

    Update Task6.java

--- 2_Algorithmization/One_dimensional_arrays/Task6.java
+++ 2_Algorithmization/One_dimensional_arrays/Task6.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class Main {
+public class Task6 {
     public static void main(String args[]) throws IOException {
         double array[] = {12.1, 13.2, 14.3, 15.4, 16.5, 17.6, 18.7, 19.8, 20.9, 21.12};
         double sum = 0;
