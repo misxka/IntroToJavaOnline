3e976e445864c9cb620dc76c99046c4554f7d26e
misxka <57208499+misxka@users.noreply.github.com>
2020-07-27 11:24:51 +0300

    Update Task3.java

--- 2_Algorithmization/One_dimensional_arrays/Task3.java
+++ 2_Algorithmization/One_dimensional_arrays/Task3.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class Main {
+public class Task3 {
     public static void main(String args[]) throws IOException {
         double array[] = {12.1, -13.2, -14.3, -15.4, 0, -17.6, 0, 19.8, 20.9, 21.12};
         int positiveSum = 0, negativeSum = 0, nullSum = 0;
